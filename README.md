9213ab14c187311bff5a55c7177dd7719c81046a
fabjanvucina <fabjanvucina@gmail.com>
2022-10-22 18:57:27 +0200

    Update README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+This is an example Jekyll project used in Codeanywhere.
+
+[Try it out](https://beta.codeanywhere.com/workspace#https://github.com/Codeanywhere-Templates/jekyll)
+### Want to contribute?
+
+Feel free to [open a PR](https://github.com/Codeanywhere-Templates/jekyll) with any suggestions for this test project 😃 
---
+++ test-project/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd $(dirname "$0")
+
+source test-utils.sh vscode
+
+# Run common tests
+checkCommon
+
+# Image specific tests
+check "jekyll" jekyll --version
+check "gem" gem --version
+check "ruby" ruby --version
+check "bundler" bundler --version
+check "github-pages" github-pages --version
+check "nvm" bash -c ". /usr/local/share/nvm/nvm.sh && nvm install 10"
+check "nvm-node" bash -c ". /usr/local/share/nvm/nvm.sh && node --version"
+
+# Report result
+reportResults
